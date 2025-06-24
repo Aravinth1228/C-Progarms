f1b47a0cfcdbbe4274c6a025ecada121380e620c
Aravinth1228 <aravinthkalai1228@gmial.com>
2025-06-24 16:07:35 +0530

    daily commit

---
+++ String/string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char c[100];
+    printf("Enter the String:" );
+    gets(c);
+ 
+    printf("\nReverse : %s ",strrev(c) );
+   
+    return 0;
+}
