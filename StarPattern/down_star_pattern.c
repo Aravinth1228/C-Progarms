d39cc8b8dd69ca0061729f40d654d5c24a3ad602
Aravinth1228 <aravinthkalai1228@gmial.com>
2025-06-25 13:09:28 +0530

    add

--- StarPattern/down_star_pattern.c
+++ StarPattern/down_star_pattern.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main(){
-    int i,j,n;
+    int i, j, n;
     printf("Enter the N value:");
-    scanf("%d",&n);
-    for(i=0;i<=n;i++){
-         for(j=-n;j<=n;j++){
-             if(-(n-i)<j && j<(n-i))
-                 printf(" ");
-             else
-                 printf(" *");
-         }
-         printf("\n");
+    scanf("%d", &n);
+    for(i = 0; i <= n; i++){
+        for(j = -n; j <= n; j++){
+            if(-(n - i) < j && j < (n - i))
+                printf("  "); // two spaces for alignment
+            else
+                printf("* ");
+        }
+        printf("\n");
     }
     return 0;
 }
