d3844eb2c0d41a9653d9f35c0a4535d5eece6a4d
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-07-02 15:58:53 +0530

    add

---
+++ DSA/Quick_sort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,temp;
+    printf("Enter the size:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the A Array:\n");
+     for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+
+     }
+     int size = sizeof(a)/sizeof(a[0]);
+  
+     for (i=0;i<size ;i++){
+        for (j=i+1;j<size ;j++){
+            if(a[i]>a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+     }
+
+    printf("The sorted array is:");
+    for(i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+}
---
+++ DSA/selection_sort.c
@@ -0,0 +1,34 @@
+//selection sort
+#include <stdio.h>
+int main(){
+    int n, i,j, temp;
+    printf("Enter the Size :");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter the Values :");
+    for(i=0;i<n;i++){
+        scanf("%d", &a[i]);
+            }
+    for(i=0;i<n-1;i++){
+        for(j=i;j<n;j++){
+            if(a[i]>a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("The sorted array is:");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
+/*Enter the Size :5
+Enter the Values :5
+8
+6
+1
+7
+The sorted array is:1 5 6 7 8
+*/
