d32bf0f2899743041dd8ebc76fd2724c53af9750
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-07-21 14:09:07 +0530

    Add

---
+++ DSA/Queue/circular queue.c
@@ -0,0 +1,67 @@
+//Circular Queue Implementation in C
+#include <stdio.h>
+#define MAX 100
+int queue[MAX];
+int front = -1, rear = -1;
+
+int enqueue(int value){
+    if((rear + 1) % MAX == front){
+        printf("Queue is full\n");
+        return -1;
+    }
+    if (front == -1) {
+        front = 0; 
+    }
+    rear = (rear + 1) % MAX;
+    queue[rear] = value;
+    return 0;
+
+}
+int dequeue(){
+  
+    int data = queue[front];
+    if(front == rear){ 
+        front = rear = -1;
+    } else {
+        front = (front + 1) % MAX;
+    }
+    printf("Dequeued value: %d\n", data);
+    return data;
+}
+int display(){
+    if (front == -1) {
+        printf("Queue is empty\n");
+        return 0;
+    }
+    int i = front;
+    do {
+        printf("%d -> ", queue[i]);
+        i = (i + 1) % MAX;
+    } while (i != (rear + 1) % MAX);
+    printf("NULL\n");
+    return 0;
+}
+int main(){
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    for(int i = 0; i < num; i++){
+        int value;
+        scanf("%d", &value);
+        enqueue(value);
+    }
+    display();
+    dequeue();
+    display();
+}
+
+/*Enter the number: 5
+1
+2
+3
+4
+5
+1 -> 2 -> 3 -> 4 -> 5 -> NULL
+Dequeued value: 1
+2 -> 3 -> 4 -> 5 -> NULL
+*/
