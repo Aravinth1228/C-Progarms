801c163ab4bca36041b224735385da55cce1a9f9
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-07-30 08:59:03 +0530

    Add

--- DSA/Tree/Binary search tree.c
+++ DSA/Tree/Binary search tree.c
@@ -22,54 +22,42 @@ void createNode(node** root) {
     printf("Enter the right node %d:\n", data);
     createNode(&((*root)->right));
 }
-
 void display_inorder(node* root) {
-    if (root == NULL) return;
+    if (root == NULL) {
+        return;
+    }
     display_inorder(root->left);
     printf("%d ", root->data);
     display_inorder(root->right);
 }
-
 void display_postorder(node* root) {
-    if (root == NULL) return;
+    if (root == NULL) {
+        return;
+    }
     display_postorder(root->left);
     display_postorder(root->right);
     printf("%d ", root->data);
 }
-
 void display_preoder(node* root) {
-    if (root == NULL) return;
+    if (root == NULL) {
+        return;
+    }
     printf("%d ", root->data);
     display_preoder(root->left);
     display_preoder(root->right);
 }
 
+void delete(){
+    
+}
 int main(){
-    int pre_order[100], in_order[100], post_order[100];
-    int n, i = 0;
     node* root = NULL;
-    printf("Enter the number of nodes: ");
-    scanf("%d", &n);
-    
-    printf("Enter the in-order :\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &in_order[i]);
-    }
-
-    printf("Enter the post-order :\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &post_order[i]);
-    }
-    printf("Enter the pre-order :\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &pre_order[i]);
-    }
-    
+    createNode(&root);
     printf("In-order traversal of the tree:\n");
     display_inorder(root);
-    printf("\nPost-order traversal of the tree:\n");
     display_postorder(root);
-    printf("\nPre-order traversal of the tree:\n");
     display_preoder(root);
     printf("\n");
+    
+    return 0;
 }
